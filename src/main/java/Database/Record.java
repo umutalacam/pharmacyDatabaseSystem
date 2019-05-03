66d8db8d0de2a21a94f5563270abbe2a7440c700
umutalacam <umutcanalacam@gmail.com>
2019-05-03 14:08:31 +0300

    Add New Inventory Method avaible

--- src/main/java/Database/Record.java
+++ src/main/java/Database/Record.java
@@ -4,10 +4,32 @@ import Unit.Drug;
 import Unit.Inventory;
 import Unit.Patient;
 import Unit.Pharmacy;
+import com.mysql.jdbc.Connection;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Record {
 
-    public static boolean addInventory(Pharmacy pharmacy, Inventory inventory){
+    public static boolean addNewInventory(Pharmacy pharmacy) {
+        int phar_id = pharmacy.getPhar_id();
+        int drug_id = 0;
+        int quantity = 0;
+        Date expiry_date = new Date(System.currentTimeMillis());
+        String sql = "INSERT INTO Inventory (phar_id, drug_id, quantity, expiry_date) VALUES(?, ?, ?, ?)";
+        Connection conn = Database.Connector.connect();
+        if (conn == null) return false;
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1,phar_id);
+            stmt.setInt(2,0);
+            stmt.setInt(3,0);
+            stmt.setDate(4, expiry_date);
+            return stmt.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import Database.Record;
+import Unit.Inventory;
+import Unit.Pharmacy;
+
+import java.sql.Connection;
 
 public class Main {
+
+
+    public static void main(String[] args) {
+        Connection conn = Database.Connector.connect();
+        Pharmacy pharmacy = new Pharmacy(1, "Faruk Pharmacy",
+                "2200 Eskisehir", 837523);
+
+        Record.addNewInventory(pharmacy);
+    }
 }
